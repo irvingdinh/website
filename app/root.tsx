44cc1b0ac1f1740f1aed5439e203597307534f4c
Irving Dinh <irving.dinh@gmail.com>
2024-12-01 01:41:14 +0700

    Initial commit (cont.)

--- app/root.tsx
+++ app/root.tsx
@@ -10,14 +10,14 @@ import {
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-white text-black dark:bg-black dark:text-white">
         {children}
         <ScrollRestoration />
         <Scripts />
